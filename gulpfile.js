f03b0b644ea89c7ba892b15df840f2a06c9d5208
YannickDot <yannick.levif@gmail.com>
2014-08-29 01:16:38 +0200

    added clean task and html boilerplate file

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ var concat = require('gulp-concat');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
+var rimraf = require('gulp-rimraf');
+
 var reload = browserSync.reload;
 
 var paths = {
 
-  dist : 'dist/',
+  dist : 'dist/**',
 
   output : {
     js : 'dist/js',
@@ -27,12 +29,17 @@ var paths = {
     images : ['src/img/**/*'],
   },
 
-  server : './src',
+  server : './dist',
 
   test: [
     'test/spec/**/*.js'
   ]
 };
+
+gulp.task('clean', function() {
+  return gulp.src(paths.dist, { read: false }) // much faster
+    .pipe(rimraf());
+});
 
 
 gulp.task('js', function () {
@@ -80,14 +87,15 @@ gulp.task('bs-reload', function () {
     browserSync.reload();
 });
 
-gulp.task('build', function() {
+gulp.task('build', ['clean'], function() {
     gulp.start('sass', 'js', 'templates', 'index', 'images');
 });
 
-gulp.task('default', ['browser-sync'], function () {
+gulp.task('default', ['build', 'browser-sync'], function () {
 
   	gulp.watch(paths.input.index, ['index', 'bs-reload']); //ok
   	gulp.watch(paths.input.templates, ['templates', 'bs-reload']); //ok
   	gulp.watch(paths.input.sass, ['sass']); //ok - TODO : install sass
     gulp.watch(paths.input.js, ['js', 'bs-reload']); //ok
+    gulp.watch(paths.input.images, ['images', 'bs-reload']); //ok
 });
