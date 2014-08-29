7a970ad0104e45a4bb3900e3aeb10546c60a5e2a
YannickDot <yannick.levif@gmail.com>
2014-08-29 03:59:27 +0200

    fix css injection with sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
-var cache = require('gulp-cache');
-//var sass = require('gulp-sass');
-var sass = require('gulp-ruby-sass');
-var imagemin = require('gulp-imagemin');
-var rimraf = require('gulp-rimraf');
+var gulp        = require('gulp');
+var jshint      = require('gulp-jshint');
+var uglify      = require('gulp-uglify');
+var concat      = require('gulp-concat');
+var cache       = require('gulp-cache');
+var rename      = require('gulp-rename');
+var filter      = require('gulp-filter');
+//var sass      = require('gulp-sass');
+var sass        = require('gulp-ruby-sass');
+var imagemin    = require('gulp-imagemin');
+var rimraf      = require('gulp-rimraf');
 var browserSync = require('browser-sync');
-
-var reload = browserSync.reload;
+var reload      = browserSync.reload;
 
 var paths = {
 
@@ -44,6 +45,15 @@ gulp.task('clean', function() {
 });
 
 
+gulp.task('js-dev', function () {
+   return gulp.src(paths.input.js)
+      .pipe(jshint())
+      .pipe(jshint.reporter('jshint-stylish'))
+      .pipe(uglify())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest(paths.output.js));
+});
+
 gulp.task('js', function () {
    return gulp.src(paths.input.js)
       .pipe(jshint())
@@ -53,14 +63,23 @@ gulp.task('js', function () {
       .pipe(gulp.dest(paths.output.js));
 });
 
+gulp.task('sass-dev', function () {
+    return gulp.src(paths.input.sass)
+        .pipe(sass({style: 'compressed'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.output.css))
+        .pipe(filter('**/*.css'))
+        .pipe(reload({stream:true}));
+});
+
 gulp.task('sass', function () {
     return gulp.src(paths.input.sass)
         .pipe(sass({style: 'compressed'}))
         .pipe(concat('style.css'))
         .pipe(gulp.dest(paths.output.css))
+        .pipe(filter('**/*.css'))
         .pipe(reload({stream:true}));
 });
-
 
 gulp.task('index', function () {
     return gulp.src(paths.input.index)
@@ -90,14 +109,18 @@ gulp.task('bs-reload', function () {
     browserSync.reload();
 });
 
+gulp.task('build-dev', ['clean'], function() {
+    gulp.start('sass-dev', 'js-dev', 'templates', 'index', 'images');
+});
+
 gulp.task('build', ['clean'], function() {
     gulp.start('sass', 'js', 'templates', 'index', 'images');
 });
 
-gulp.task('default', ['build', 'browser-sync'], function () {
-    gulp.watch(paths.input.index, ['index', 'bs-reload']); //ok
-    gulp.watch(paths.input.templates, ['templates', 'bs-reload']); //ok
-    gulp.watch(paths.input.sass, ['sass']); //ok - TODO : install sass
-    gulp.watch(paths.input.js, ['js', 'bs-reload']); //ok
-    gulp.watch(paths.input.images, ['images', 'bs-reload']); //ok
+gulp.task('default', ['build-dev', 'browser-sync'], function () {
+    gulp.watch(paths.input.index, ['index', 'bs-reload']); 
+    gulp.watch(paths.input.templates, ['templates', 'bs-reload']); 
+    gulp.watch(paths.input.sass, ['sass-dev']); 
+    gulp.watch(paths.input.js, ['js-dev', 'bs-reload']); 
+    gulp.watch(paths.input.images, ['images', 'bs-reload']); 
 });
